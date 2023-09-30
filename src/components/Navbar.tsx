f7dab8c86fc5734b78ded57fe8a7f541351dd020
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-09-30 13:43:52 +0530

    navbar and authentication

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
+import { FC } from 'react';
+import MaxWidthWrapper from './MaxWidthWrapper';
+import { buttonVariants } from './ui/button';
+
+interface NavbarProps {}
+
+const Navbar: FC<NavbarProps> = ({}) => {
+    return (
+        <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+            <MaxWidthWrapper>
+                <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+                    <Link href="/" className="flex z-40 font-semibold">
+                        <span>down.</span>
+                    </Link>
+
+                    <div className="hidden items-center space-x-4 sm:flex">
+                        <>
+                            <Link
+                                href="/pricing"
+                                className={buttonVariants({
+                                    variant: 'ghost',
+                                    size: 'sm',
+                                })}
+                            >
+                                Pricing
+                            </Link>
+                            <LoginLink
+                                className={buttonVariants({
+                                    variant: 'ghost',
+                                    size: 'sm',
+                                })}
+                            >
+                                Sign In
+                            </LoginLink>
+                            <RegisterLink
+                                className={buttonVariants({
+                                    size: 'sm',
+                                })}
+                            >
+                                Get Started{' '}
+                                <ArrowRight className="ml-1.5 h-5 w-5" />
+                            </RegisterLink>
+                        </>
+                    </div>
+                </div>
+            </MaxWidthWrapper>
+        </nav>
+    );
+};
+
+export default Navbar;
