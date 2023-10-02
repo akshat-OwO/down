fd7f94c1b68b0417a43cc80dc8ed28d0977ce518
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-10-02 16:26:26 +0530

    Creating the PDF Renderer

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -33,7 +33,7 @@ const page: FC<pageProps> = async ({ params }) => {
         <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
             <div className='flex-1 xl:flex'>
                 <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
-                    <PdfRenderer />
+                    <PdfRenderer url={file.url} />
                 </div>
             </div>
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import 'react-loading-skeleton/dist/skeleton.css';
+import 'simplebar-react/dist/simplebar.min.css';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
