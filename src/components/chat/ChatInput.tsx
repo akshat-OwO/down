e1aceabfd93a4338d237674d469912f94b211575
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-10-05 11:03:42 +0530

    creating messages section and instant loading states

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+interface ChatInputProps {
+    isDisabled?: boolean;
+}
+
+const ChatInput: FC<ChatInputProps> = ({ isDisabled }) => {
+    return <div>ChatInput</div>;
+};
+
+export default ChatInput;
---
+++ src/components/chat/Messages.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface MessagesProps {
+  
+}
+
+const Messages: FC<MessagesProps> = ({}) => {
+  return <div></div>
+}
+
+export default Messages
