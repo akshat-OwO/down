ec92e9c514ef1e97ec775f812b2f371155b4aec6
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-10-06 17:51:48 +0530

    chat input, contexts and streaming api response in real time

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,9 @@
 import { db } from '@/db';
+import { pinecone } from '@/lib/pinecone';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 
 const f = createUploadthing();
@@ -24,6 +28,48 @@ export const ourFileRouter = {
                     uploadStatus: 'PROCESSING',
                 },
             });
+
+            try {
+                const response = await fetch(
+                    `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+                );
+                const blob = await response.blob();
+
+                const loader = new PDFLoader(blob);
+
+                const pageLevelDocs = await loader.load();
+
+                const pagesAmt = pageLevelDocs.length;
+
+                const pineconeIndex = pinecone.Index('down');
+
+                const embeddings = new OpenAIEmbeddings({
+                    openAIApiKey: process.env.OPENAI_API_KEY,
+                });
+
+                await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+                    pineconeIndex,
+                    namespace: createdFile.id,
+                });
+
+                await db.file.update({
+                    data: {
+                        uploadStatus: 'SUCCESS',
+                    },
+                    where: {
+                        id: createdFile.id,
+                    },
+                });
+            } catch (error) {
+                await db.file.update({
+                    data: {
+                        uploadStatus: 'FAILED',
+                    },
+                    where: {
+                        id: createdFile.id,
+                    },
+                });
+            }
         }),
 } satisfies FileRouter;
 
